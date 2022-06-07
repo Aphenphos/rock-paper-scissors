6930b9403917a8fa2bdd3fb21dba816271791979
Aphenphos <MaxwillWinters@gmail.com>
2022-06-06 23:02:23 -0400

    got things to almost working state. Though confused

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import { getRandomItem } from "./utils";
+import { getRandomItem } from './utils';
 
 
 
 const throwOptions = ['rock', 'paper', 'scissors'];
 
-let userPoints = 0;
+let playerPoints = 0;
 let computerPoints = 0;
 let playerThrow = '';
 let computerThrow = '';
@@ -13,44 +13,37 @@ const rockButton = document.getElementById('rock-button');
 const paperButton = document.getElementById('paper-button');
 const scissorsButton = document.getElementById('scissors-button');
 
-function determineGame {
-    
-    playerThrow = getRandomItem(throwOptions)
-    if (playerThrow === computerThrow) {return 'tie'}
-    if (playerThrow === rock){
-        if(computerThrow === scissors) return 'player wins'
+function determineGame(playerThrow) { 
+    computerThrow = getRandomItem(throwOptions);
+    if (playerThrow === computerThrow) {return 'tie';}
+    if (playerThrow === 'rock'){
+        if (computerThrow === 'scissors') return 'player wins';
     } else {
-        return "computer wins"
+        return 'computer wins';
     }
-    if (playerThrow === paper){
-        if(computerThrow === scissors) return 'computer wins'
+    if (playerThrow === 'scissors'){
+        if (computerThrow === 'paper') return 'player wins';
     } else {
-        return "player wins"
+        return 'computer wins';
     }
-    if (playerThrow === scissors){
-        if(computerThrow === scissors) return 'player wins'
+    if (playerThrow === 'paper'){
+        if (computerThrow === 'rock') return 'player wins';
     } else {
-        return "computer wins"
-    }
+        return 'computer wins';}
+    console.log(computerThrow);
 }
-
-
-const throwMap = new Map ();
-throwMap.set('rock', rockButton);
-throwMap.set('paper', paperButton);
-throwMap.set('scissors', scissorsButton);
-
 
 rockButton.addEventListener('click', () => {
     determineGame('rock');
-})
+    console.log('clicked');
+});
 
 scissorsButton.addEventListener('click', () => {
     determineGame('paper');
-})
+});
 
 paperButton.addEventListener('click', () => {
     determineGame('scissors');
-})
+});
 
 
